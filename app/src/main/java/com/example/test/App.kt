157491a3849e6ef9fdd7defa344f9893e5d7fb2d
Artistle <nikitaobryvkov39@gmail.com>
2021-10-17 00:18:20 +0300

    комментарии к проекту

--- app/src/main/java/com/example/test/App.kt
+++ app/src/main/java/com/example/test/App.kt
@@ -4,6 +4,20 @@ import android.app.Application
 import com.example.test.dependencies.AppComponent
 import com.example.test.dependencies.DaggerAppComponent
 
+
+/**
+*
+*
+ * Не лучшее решение, из проблем в проекте я могу выделить:
+ *      плохой нэйминг, особенно с фрагментами.
+ *      не успел придумать что делать с модулем для вью-модели, он должен быть 1 экземпляре
+ *      по хорошему надо бы ещё проработать архитектуру, избавить от composible во вью моделях, и делатьб запросы только в слое репозитория
+ *      но сделал как смог за такой короткий срок.
+ *      в общей сумме потратил ~10-12 часов
+*
+* */
+
+
 class App : Application() {
 
     companion object {
