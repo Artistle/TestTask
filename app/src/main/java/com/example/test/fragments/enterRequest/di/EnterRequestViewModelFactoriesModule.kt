426d93e951c36e02e57738bc61531111bd90dbb4
Artistle <nikitaobryvkov39@gmail.com>
2021-10-17 01:10:13 +0300

    hotfix

--- app/src/main/java/com/example/test/fragments/enterRequest/di/EnterRequestViewModelFactoriesModule.kt
+++ app/src/main/java/com/example/test/fragments/enterRequest/di/EnterRequestViewModelFactoriesModule.kt
@@ -3,6 +3,7 @@ package com.example.test.fragments.enterRequest.di
 import androidx.lifecycle.ViewModel
 import com.example.test.dependencies.viewmodel.AssistedSavedStateViewModelFactory
 import com.example.test.dependencies.viewmodel.ViewModelKey
+import com.example.test.fragments.answerFragment.viewmodel.AnswerViewModel
 import com.example.test.fragments.enterRequest.viewmodel.EnterRequestViewModel
 import com.squareup.inject.assisted.dagger2.AssistedModule
 import dagger.Binds
@@ -10,11 +11,18 @@ import dagger.Module
 import dagger.multibindings.IntoMap
 
 @AssistedModule
-@Module(includes = [AssistedInject_EnterRequestViewModelFactoriesModule::class])
+@Module(
+    includes = [AssistedInject_EnterRequestViewModelFactoriesModule::class]
+)
 interface EnterRequestViewModelFactoriesModule {
 
     @Binds
     @IntoMap
     @ViewModelKey(EnterRequestViewModel::class)
     fun bindEnterRequestViewModelFactory(impl: EnterRequestViewModel.Factory): AssistedSavedStateViewModelFactory<out ViewModel>
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(AnswerViewModel::class)
+    fun bindAnswerViewModelFactory(impl: AnswerViewModel.Factory): AssistedSavedStateViewModelFactory<out ViewModel>
 }
